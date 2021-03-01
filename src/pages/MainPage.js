749359e8e3692d3777a4c7ddc3516491d7231a63
romantymets <romantymets@gmail.com>
2021-03-01 18:57:11 +0200

    add eslint

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { addTodo as addTodoAction } from '../redux/todosReducer/todosReducer';
+import React, { useEffect } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { addTodo as addTodoAction } from "../redux/todosReducer/todosReducer";
 
 const MainPage = ({ todos, addTodo }) => {
   const todo = todos[0] || {};
@@ -9,24 +9,19 @@ const MainPage = ({ todos, addTodo }) => {
   useEffect(() => {
     setTimeout(() => {
       addTodo({
-        title: 'hi'
-      })
-    }, 2000)
-  },[]);
-
+        title: "hi",
+      });
+    }, 2000);
+  }, []);
 
   return (
     <div>
       <h2> Helo </h2>
       <p>{todo.title}</p>
     </div>
-  )
+  );
 };
 
-
 export default compose(
-  connect(
-    state => ({ todos: state.todos }),
-    { addTodo: addTodoAction }
-  ),
+  connect((state) => ({ todos: state.todos }), { addTodo: addTodoAction })
 )(MainPage);
