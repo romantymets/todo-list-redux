602622781b2ecf4a2f500b405d10d3256d116203
romantymets <romantymets@gmail.com>
2021-03-07 19:09:13 +0200

    delete todo

--- src/containers/Todo/component/List/List.js
+++ src/containers/Todo/component/List/List.js
@@ -4,22 +4,32 @@ import { connect } from "react-redux";
 // import {useHistory} from "react-router-dom";
 import classNames from "classnames";
 import style from "./List.module.css";
+import { deleteTodo as deleteOldTodo } from "../../../../redux/todosReducer/todosReducer";
 
 // eslint-disable-next-line react/prop-types
-function List({ todos }) {
+function List({ todos, deleteTodo }) {
   // const history = useHistory();
-
   // const onItemClick = (todo) => () => {
   //   history.push(`/todoitem${todo._id}`);
   // };
+  const onDeleteTodo = (id) => {
+    // eslint-disable-next-line react/prop-types,no-unused-vars
+    const findeIndexlement = todos.findIndex((todo) => todo.id === id);
+    const newtodos = todos;
+    newtodos.splice(findeIndexlement, 1);
+    deleteTodo(newtodos);
+  };
 
   return (
     <ul className="list-group list-group-flush">
       {/* eslint-disable-next-line react/prop-types */}
       {todos.map((todo, index) => (
         <li
-          className="list-group-item"
-          key={todo._id}
+          className={classNames(
+            style.listItem,
+            `${todo.completed ? style.doneTodo : null}`
+          )}
+          key={todo.id}
           // onClick={onItemClick(todo)}
         >
           <div className="container">
@@ -33,17 +43,18 @@ function List({ todos }) {
                 <input
                   type="checkbox"
                   checked={todo.completed}
-                  // onChange={onItemCheck(todo._id)}
+                  className={style.checkboxContainer}
+                  // onChange={onItemCheck(todo.id)}
                 />
               </div>
               <div className="col-1"> {index + 1}</div>
               <div className="col-9"> {todo.title}</div>
               <div className="col-1">
                 <button
-                  className="btn-danger"
-                  // onClick={() => {
-                  //   deleteTodo(todo._id);
-                  // }}
+                  className={classNames("btn-danger", style.button)}
+                  onClick={() => {
+                    onDeleteTodo(todo.id);
+                  }}
                 >
                   {" "}
                   X
@@ -58,7 +69,12 @@ function List({ todos }) {
 }
 
 export default compose(
-  connect((state) => ({
-    todos: state.todos,
-  }))
+  connect(
+    (state) => ({
+      todos: state.todos,
+    }),
+    {
+      deleteTodo: deleteOldTodo,
+    }
+  )
 )(List);
--- src/redux/todosReducer/todosReducer.js
+++ src/redux/todosReducer/todosReducer.js
@@ -5,12 +5,18 @@ export { initialState };
 
 // Actions
 export const ADD_TODO = `${ROOT_PREFIX}ADD_TODO`;
-
+export const DELETE_TODO = `${ROOT_PREFIX}DELETE_TODO`;
 // Action creators
 export const addTodo = (todo) => (dispatch) => {
   dispatch({
     type: ADD_TODO,
     todo: todo,
+  });
+};
+export const deleteTodo = (newtodos) => (dispatch) => {
+  dispatch({
+    type: DELETE_TODO,
+    preload: newtodos,
   });
 };
 
@@ -19,6 +25,9 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO: {
       return [...state, action.todo];
+    }
+    case DELETE_TODO: {
+      return (state = [...action.preload]);
     }
     default:
       return state;
