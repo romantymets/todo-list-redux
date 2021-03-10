77b24f31e1c7a0c0ebd5051651d8dd7c544f6065
romantymets <romantymets@gmail.com>
2021-03-10 21:25:44 +0200

    added done todo_list

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import FormContainer from "../../containers/Todo/FormConteiner";
-import TodoCompletedForm from "../../containers/TodoCompled/TodoCompletedForm";
+import FormContainer from "../../containers/TodoInprocesForm/FormConteiner";
+import TodoCompletedForm from "../../containers/TodoCompledForm/TodoCompletedForm";
 import "./MainPage.css";
 
 const MainPage = () => {
